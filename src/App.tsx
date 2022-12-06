3c67d5b8d19d10d8d2d34b592bdb079f91a75b9e
Celine Sarafa <celinesarafa@gmail.com>
2022-12-06 13:46:42 +0300

    Cleanup unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/layout/Header";
-import Main from "./components/layout/Main";
 import Sidebar from "./components/layout/Sidebar";
-import TargetSelector from "./components/layout/TargetSelector";
 import AuthProtectedPage from "./components/utils/AuthProtectedPage";
 
 function App() {
