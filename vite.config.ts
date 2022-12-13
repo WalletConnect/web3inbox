ad6a161beb32ac00169f07646cf10b5ee917cb9b
Celine Sarafa <celinesarafa@gmail.com>
2022-12-13 11:46:28 +0300

    Update icons for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -16,9 +16,20 @@ const pwaOptions: Partial<VitePWAOptions> = {
     theme_color: '#ffffff',
     icons: [
       {
-        src: '/logo.png',
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
         sizes: '512x512',
         type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
       }
     ]
   },
