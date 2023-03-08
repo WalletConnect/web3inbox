7614545102710f2b5332f86ddd4592500d1f91a1
Celine Sarafa <celinesarafa@gmail.com>
2023-03-08 16:09:03 +0300

    Feat/improved signin flow (#52)
    * Improve signin flow by making signature explicit
    
    * Add styling to signature modal
    
    * Add failsafe for change account
    
    * Add typo

--- src/pages/Login/SignatureModal/index.tsx
+++ src/pages/Login/SignatureModal/index.tsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import { signMessage } from '@wagmi/core'
+import React, { useCallback } from 'react'
+import Button from '../../../components/general/Button'
 import { Modal } from '../../../components/general/Modal/Modal'
 import { signatureModalService } from '../../../utils/store'
+import { formatJsonRpcRequest } from '@walletconnect/jsonrpc-utils'
 import './SignatureModal.scss'
 
-export const SignatureModal: React.FC = () => {
+export const SignatureModal: React.FC<{ message: string }> = ({ message }) => {
+  const onSign = useCallback(() => {
+    signMessage({ message }).then(signature => {
+      window.web3inbox.chat.postMessage(
+        formatJsonRpcRequest('chat_signature_delivered', { signature })
+      )
+    })
+  }, [message])
+
   return (
     <Modal onToggleModal={signatureModalService.toggleModal}>
       <div className="SignatureModal">
         <div className="SignatureModal__header">
           <h2>Signature requested</h2>
         </div>
+        <div className="SignatureModal__explanation">
+          <p>You need to perform a final signature to establish an identity key.</p>
+        </div>
         <div className="SignatureModal__content">
-          <div className="SignatureModal__content__title">
-            Please sign the message on your wallet.
-          </div>
+          <Button onClick={onSign}>Sign Message</Button>
         </div>
       </div>
     </Modal>
