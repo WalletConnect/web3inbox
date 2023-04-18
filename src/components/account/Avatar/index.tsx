fb0f21d49535a33b8e643f5a905ae28725004d5f
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2023-04-18 12:40:35 +0530

    feat(address): Added functionality for address copy

--- src/components/account/Avatar/index.tsx
+++ src/components/account/Avatar/index.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useBalance, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi'
 import { toast } from 'react-toastify'
 
-import Disconnect from '../../../assets/Disconnect.svg'
+import DisconnectIcon from '../../general/Icon/DisconnectIcon'
 import ETH from '../../../assets/ETH.svg'
 import W3iContext from '../../../contexts/W3iContext/context'
 import { useIsMobile, useOnClickOutside } from '../../../utils/hooks'
@@ -14,6 +14,9 @@ import Divider from '../../general/Divider'
 
 import './Avatar.scss'
 import SettingsContext from '../../../contexts/SettingsContext/context'
+import PersonIcon from '../../general/Icon/PersonIcon'
+import ShareIcon from '../../general/Icon/ShareIcon'
+import CopyIcon from '../../general/Icon/CopyIcon'
 
 interface AvatarProps {
   address?: string
@@ -140,19 +143,7 @@ const Avatar: React.FC<AvatarProps> = ({ address, width, height, hasProfileDropd
                       })
                   }}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
-                    />
-                  </svg>
+                  <CopyIcon />
                 </button>
               </div>
             </div>
@@ -168,19 +159,22 @@ const Avatar: React.FC<AvatarProps> = ({ address, width, height, hasProfileDropd
             <Divider />
             <div className="Avatar__dropdown__block Avatar__dropdown__block__group">
               <div className="Avatar__dropdown__block__actions ">
-                {/* <button onClick={handleViewProfile}>
+                <button
+                  onClick={handleViewProfile}
+                  className="Avatar__dropdown__block__actions__button"
+                >
                   <PersonIcon />
                   <span>View Profile</span>
                 </button>
-                <button onClick={handleShare}>
+                <button onClick={handleShare} className="Avatar__dropdown__block__actions__button">
                   <ShareIcon />
                   <span>Share</span>
-                </button> */}
+                </button>
                 <button
-                  className="Avatar__dropdown__block__actions__disconnect"
+                  className="Avatar__dropdown__block__actions__button Avatar__dropdown__block__actions__disconnect"
                   onClick={handleDisconnect}
                 >
-                  <img alt="share-icon" src={Disconnect} />
+                  <DisconnectIcon />
                   <span>Disconnect</span>
                 </button>
               </div>
--- src/components/account/Share/Share.tsx
+++ src/components/account/Share/Share.tsx
@@ -1,11 +1,121 @@
-import React from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { shareModalService } from '../../../utils/store'
 import { Modal } from '../../general/Modal/Modal'
+import './Share.scss'
+import type { QRCodeErrorCorrectionLevel } from 'qrcode'
+// eslint-disable-next-line no-duplicate-imports
+import QRCode from 'qrcode'
+import CopyIcon from '../../general/Icon/CopyIcon'
+import SettingsContext from '../../../contexts/SettingsContext/context'
+import { useColorModeValue } from '../../../utils/hooks'
+import { toast } from 'react-toastify'
+import W3iContext from '../../../contexts/W3iContext/context'
+import { truncate } from '../../../utils/string'
 
 export const Share: React.FC = () => {
+  const { userPubkey: address } = useContext(W3iContext)
+
+  const { mode } = useContext(SettingsContext)
+  const themeColors = useColorModeValue(mode)
+
+  const toastTheme = useMemo(() => {
+    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    const specifiedMode = mode === 'system' ? systemTheme : mode
+
+    return specifiedMode
+  }, [mode])
+
+  const [qrCode, setQrCode] = useState<string | null>(null)
+
+  const qrOptions = {
+    errorCorrectionLevel: 'H' as QRCodeErrorCorrectionLevel,
+    width: 314,
+    margin: 0,
+    version: 10,
+    color: {
+      dark: themeColors['--qr-color-1'],
+      light: '#00000000'
+    }
+  }
+
+  useEffect(() => {
+    QRCode.toString('https://web3inbox.com', qrOptions)
+      .then(url => {
+        setQrCode(url)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [qrOptions])
+
+  const handleCopyClick = () => {
+    window.navigator.clipboard
+      .writeText(address ?? '')
+      .then(() => {
+        toast('Copied address to clipboard', {
+          type: 'success',
+          position: 'bottom-right',
+          autoClose: 5000,
+          theme: toastTheme,
+          style: {
+            borderRadius: '1em'
+          }
+        })
+      })
+      .catch(() => {
+        toast('Failed to copy address', {
+          type: 'error',
+          position: 'bottom-right',
+          autoClose: 5000,
+          theme: toastTheme,
+          style: {
+            borderRadius: '1em'
+          }
+        })
+      })
+  }
+
   return (
     <Modal onToggleModal={shareModalService.toggleModal}>
-      <h1>Share modal content</h1>
+      <div className="Share">
+        <div className="Share__header">
+          <button className="Share__header--back">
+            <svg
+              width="10"
+              height="20"
+              viewBox="0 0 10 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M7.23547 0.941281C7.68211 0.414878 8.47092 0.350222 8.99733 0.796866C9.52373 1.24351 9.58838 2.03232 9.14174 2.55872L3.23964 9.51477C3.00215 9.79467 3.00215 10.2053 3.23964 10.4852L9.14174 17.4413C9.58838 17.9677 9.52373 18.7565 8.99733 19.2031C8.47092 19.6498 7.68211 19.5851 7.23547 19.0587L1.33337 12.1027C0.304242 10.8898 0.304241 9.11023 1.33337 7.89733L7.23547 0.941281Z"
+                fill="currentcolor"
+              />
+            </svg>
+          </button>
+          <button className="Share__header--close" onClick={shareModalService.toggleModal}>
+            ✕
+          </button>
+        </div>
+        <div className="Share__qr">
+          {qrCode ? (
+            <img src={`data:image/svg+xml;utf8,${encodeURIComponent(qrCode)}`} />
+          ) : (
+            'Loading...'
+          )}
+        </div>
+        <div className="Share__address">
+          <p className="Share__address--label">Address</p>
+          <div className="Share__address__content">
+            {truncate(address as `0x${string}`)}
+            <button className="Share__address__content--copy" onClick={handleCopyClick}>
+              <CopyIcon />
+            </button>
+          </div>
+        </div>
+      </div>
     </Modal>
   )
 }
