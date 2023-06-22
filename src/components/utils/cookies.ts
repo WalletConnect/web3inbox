6f0115604c7b5c25f5d6b0d02c97ce5e70a61bae
Celine Sarafa <celinesarafa@gmail.com>
2023-06-22 14:15:50 +0300

    Add cookie utils and recovery

---
+++ src/components/utils/cookies.ts
@@ -0,0 +1,36 @@
+export interface Cookie {
+  key: string
+  value: string
+  samesite: 'Lax' | 'None'
+  maxAgeSeconds: number
+}
+
+export const ONE_MONTH = 60 * 60 * 24 * 30
+
+export const setCookie = (cookie: Cookie) => {
+  const { key, value, samesite, maxAgeSeconds } = cookie
+
+  const baseCookie = `${encodeURIComponent(key)}=${value}`
+
+  const cookieString = `${baseCookie}; SameSite=${samesite}; max-age=${maxAgeSeconds}; Secure;`
+
+  console.log('Setting, ', cookieString)
+
+  document.cookie = cookieString
+}
+
+export const readCookie = (cookieKey: string) => {
+  const cookies = document.cookie.split(';')
+  const foundCookie = cookies.find(value => {
+    return value.startsWith(`${encodeURIComponent(cookieKey)}=`)
+  })
+  if (foundCookie) {
+    return decodeURIComponent(foundCookie.split('=')[1])
+  }
+
+  return null
+}
+
+export const deleteCookie = (cookieKey: string) => {
+  document.cookie = `${encodeURIComponent(cookieKey)}=0;max-age=0;`
+}
--- src/w3iProxy/authProviders/internalAuthProvider.ts
+++ src/w3iProxy/authProviders/internalAuthProvider.ts
@@ -1,6 +1,7 @@
 import { getAccount, watchAccount } from '@wagmi/core'
 import type { JsonRpcRequest } from '@walletconnect/jsonrpc-types'
 import type { EventEmitter } from 'events'
+import { ONE_MONTH, readCookie, setCookie } from '../../components/utils/cookies'
 
 export default class InternalAuthProvider {
   private readonly methodsListenedTo = ['auth_set_account']
@@ -10,13 +11,35 @@ export default class InternalAuthProvider {
 
   public constructor(emitter: EventEmitter, _name = 'InternalAuthProvider') {
     this.emitter = emitter
+
     watchAccount(account => {
       // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-      if (!account.address || !window.web3inbox.chat) {
-        this.emitter.emit('auth_set_account', { account: null })
+      if (!account.address) {
+        const wagmiRestoreRaw = readCookie('wagmiRestore')
+        if (wagmiRestoreRaw) {
+          const wagmiRestore = JSON.parse(wagmiRestoreRaw) as Record<string, string>
+          Object.entries(wagmiRestore).forEach(([key, value]) => {
+            localStorage.setItem(key, value)
+          })
+          document.location.reload()
+        }
 
         return
       }
+
+      const wagmiInfo = {
+        'wagmi.store': localStorage.getItem('wagmi.store'),
+        'wagmi.injected.shimDisconnect': localStorage.getItem('wagmi.injected.shimDisconnect'),
+        'wagmi.wallet': localStorage.getItem('wagmi.wallet'),
+        'wagmi.connected': localStorage.getItem('wagmi.connected')
+      }
+
+      setCookie({
+        key: 'wagmiRestore',
+        maxAgeSeconds: ONE_MONTH,
+        samesite: 'None',
+        value: JSON.stringify(wagmiInfo)
+      })
 
       this.emitter.emit('auth_set_account', { account: account.address })
     })
