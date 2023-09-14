60b3633ecd7527edd5af53e0caee660f531212f1
Sven <fr.sven.fr@gmail.com>
2023-09-14 13:50:40 +0200

    remove react toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import 'react-toastify/dist/ReactToastify.css'
 import type { ChatClientTypes } from '@walletconnect/chat-client'
 import { Fragment, useContext, useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
