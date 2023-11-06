92326d58466154ef8f180955871085b538d6ac1e
Celine Sarafa <celinesarafa@gmail.com>
2023-11-06 12:23:41 +0100

    chore: ensure echo registration post init

--- src/w3iProxy/notifyProviders/internalNotifyProvider.ts
+++ src/w3iProxy/notifyProviders/internalNotifyProvider.ts
@@ -25,8 +25,6 @@ export default class InternalNotifyProvider implements W3iNotifyProvider {
    * to allow the observers in the facade to work seamlessly.
    */
   public initState(notifyClient: NotifyClient) {
-    // Ensure we have a registration with echo (if we need it)
-    this.ensureEchoRegistration()
 
     const subsChangedListener = async (ev: {
       params: { subscriptions: NotifyClientTypes.NotifySubscription[] }
@@ -54,6 +52,8 @@ export default class InternalNotifyProvider implements W3iNotifyProvider {
       this.emitter.emit('sync_update', {})
     })
 
+    // Ensure we have a registration with echo (if we need it)
+    this.ensureEchoRegistration()
   }
 
   // ------------------------ Provider-specific methods ------------------------
