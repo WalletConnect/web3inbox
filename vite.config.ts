8d7e92a533db886c27ee7b42b72107a1da31a2fc
Celine Sarafa <celinesarafa@gmail.com>
2023-11-07 15:09:03 +0100

    chore: cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import autoprefixer from 'autoprefixer'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -12,6 +14,12 @@ export default defineConfig(() => {
         target: 'es2020'
       }
     },
+ plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true
+    }),
+  ],
     css: {
       postcss: {
         plugins: [autoprefixer({})]
