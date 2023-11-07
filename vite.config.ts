19d11c6264d25adffdadfac923817eb17762bec8
Celine Sarafa <celinesarafa@gmail.com>
2023-11-07 15:49:10 +0100

    feat: use generateSW flag

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,47 @@ import { defineConfig } from 'vite'
 import autoprefixer from 'autoprefixer'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import type { VitePWAOptions } from 'vite-plugin-pwa'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  mode: 'production',
+  base: '/',
+  strategies: 'generateSW',
+  injectRegister: 'inline',
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.svg'],
+  srcDir: 'src',
+  filename: 'main-sw.ts',
+  manifest: {
+    name: 'Web3Inbox',
+    short_name: 'Web3Inbox',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ]
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === 'true',
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: 'module',
+    navigateFallback: 'index.html'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -19,6 +60,7 @@ export default defineConfig(() => {
     nodePolyfills({
       protocolImports: true
     }),
+   VitePWA(pwaOptions)
   ],
     css: {
       postcss: {
