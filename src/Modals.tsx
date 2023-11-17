754fed0b826fba93b5af04d4ae9f33f8d9320aee
Celine Sarafa <celinesarafa@gmail.com>
2023-11-17 17:53:55 +0300

    use single dry function for notif checking

--- src/Modals.tsx
+++ src/Modals.tsx
@@ -12,6 +12,7 @@ import PwaModal from './components/utils/PwaModal'
 import { useNotificationPermissionState } from './utils/hooks/notificationHooks'
 import NotificationPwaModal from './components/utils/NotificationPwaModal'
 import { isMobile } from './utils/ui'
+import { notificationsEnabledInBrowser } from './utils/notifications'
 
 export const Modals = () => {
   const {
@@ -30,7 +31,7 @@ export const Modals = () => {
 
   const shouldShowNotificationModal = useMemo(
     () =>
-      Boolean(Notification) &&
+      notificationsEnabledInBrowser() &&
       !explicitlyDeniedOnDesktop &&
       !isMobileButNotInstalledOnHomescreen() &&
       !notificationsEnabled &&
