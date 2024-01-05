b107fa13f11c56f65c6d10bb4cfb6a7a1097190d
enesozturk <enesozturk.d@gmail.com>
2024-01-06 00:37:58 +0300

    refactor: separate project fetching methods, update hook logics to handle errors

---
+++ src/utils/projects.ts
@@ -0,0 +1,47 @@
+import { EXPLORER_API_BASE_URL, EXPLORER_ENDPOINTS } from '@/utils/constants'
+
+const projectId: string = import.meta.env.VITE_PROJECT_ID
+
+export async function fetchFeaturedProjects<T>() {
+  const explorerUrlFeatured = new URL(EXPLORER_ENDPOINTS.projects, EXPLORER_API_BASE_URL)
+
+  explorerUrlFeatured.searchParams.set('projectId', projectId)
+  explorerUrlFeatured.searchParams.set('isVerified', 'true')
+  explorerUrlFeatured.searchParams.set('isFeatured', 'true')
+
+  try {
+    const discoverProjectsData = await fetch(explorerUrlFeatured)
+      .then(res => res.json())
+      .catch(err => console.log({ featuredProjects: err }))
+    const discoverProjects = Object.values(discoverProjectsData.projects)
+
+    return {
+      data: discoverProjects as T
+    }
+  } catch (error) {
+    throw new Error('Error fetching featured projects')
+  }
+}
+
+export async function fetchDomainProjects<T>(domain: string) {
+  if (!domain) {
+    return { data: null }
+  }
+
+  let domainProject: T | null = null
+
+  const explorerUrlAppDomain = new URL(EXPLORER_ENDPOINTS.notifyConfig, EXPLORER_API_BASE_URL)
+  explorerUrlAppDomain.searchParams.set('projectId', projectId)
+  explorerUrlAppDomain.searchParams.set('appDomain', domain)
+
+  try {
+    const domainProjectsData = await fetch(explorerUrlAppDomain).then(res => res.json())
+    domainProject = domainProjectsData.data as T
+
+    return {
+      data: domainProject
+    }
+  } catch (error) {
+    throw new Error('Error fetching domain project')
+  }
+}
