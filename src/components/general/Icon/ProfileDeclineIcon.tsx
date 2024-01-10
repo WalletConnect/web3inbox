8566ac8eeb8c91fec32e0229837ab7df25d171cc
Celine Sarafa <celinesarafa@gmail.com>
2024-01-10 13:40:30 +0100

    hotfix: fix issue where subscriptions load infinitely (#365)
    * fix: infinite loading for subscribe
    
    * chore: replace svg props

--- src/components/general/Icon/ProfileDeclineIcon.tsx
+++ src/components/general/Icon/ProfileDeclineIcon.tsx
@@ -8,7 +8,7 @@ const ProfileDeclineIcon: React.FC = () => {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="1.67"
-        clip-path="url(#a)"
+        clipPath="url(#a)"
       >
         <path d="m20.59 7.42 2.83-2.83m0 2.83-2.83-2.83M14 14a2.92 2.92 0 1 0 0-5.83A2.92 2.92 0 0 0 14 14ZM8.75 18.67a2.91 2.91 0 0 1 2.67-1.75h5.16a2.91 2.91 0 0 1 2.67 1.75" />
         <path d="M24.01 10.91a10.31 10.31 0 0 1-1.28 8.92A10.5 10.5 0 1 1 17.09 4" />
