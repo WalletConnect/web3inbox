702debdb06dffc7cfe5ac674b8fb8aa379b8422a
Celine Sarafa <celinesarafa@gmail.com>
2024-01-10 16:15:16 +0100

    chore: keep noop

--- src/contexts/W3iContext/hooks/notifyHooks.ts
+++ src/contexts/W3iContext/hooks/notifyHooks.ts
@@ -49,6 +49,9 @@ export const useNotifyState = (w3iProxy: Web3InboxProxy, proxyReady: boolean) =>
   // it takes time for handshake (watch subscriptions) to complete
   // load in progress state using interval until it is
   useEffect(() => {
+    if (watchSubscriptionsComplete) {
+      return noop
+    }
     // Account for sync init
     const intervalId = setInterval(() => {
       if(notifyClient?.isInitialSubscriptionLoadComplete()) {
