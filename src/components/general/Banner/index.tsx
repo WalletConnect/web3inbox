92802591a848fabe305676812739449036efbbb5
enesozturk <enesozturk.d@gmail.com>
2024-01-11 12:54:19 +0300

    feat: add close functionality to launch banner

--- src/components/general/Banner/index.tsx
+++ src/components/general/Banner/index.tsx
@@ -8,13 +8,16 @@ import './Banner.scss'
 type BannerProps = {
   children: React.ReactNode
   className?: string
+  onClose?: () => void
 }
 
-export default function Banner({ children, className }: BannerProps) {
+export default function Banner({ children, className, onClose }: BannerProps) {
   return (
     <div className={cn('Banner', className)}>
       <Text variant="small-400">{children}</Text>
-      <CrossIcon className="Banner__close-icon" />
+      <button className="Banner__close-button" onClick={onClose}>
+        <CrossIcon className="Banner__close-icon" />
+      </button>
     </div>
   )
 }
--- src/components/general/LaunchBanner/index.tsx
+++ src/components/general/LaunchBanner/index.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react'
+
 import { Link } from 'react-router-dom'
 
 import Banner from '@/components/general/Banner'
 import ChevronRightIcon from '@/components/general/Icon/ChevronRightIcon'
+import { localStorageKeys } from '@/constants/localStorage'
 
 import './LaunchBanner.scss'
 
 const LAUNCH_BANNER_URL = 'https://walletconnect.com/blog/introducing-web3inbox-app'
 
+const isBannerClosed = localStorage.getItem(localStorageKeys.launchBannerClosed) === 'true'
+
 export default function LaunchBanner() {
+  const [closed, setClosed] = useState(isBannerClosed)
+
+  if (closed) {
+    return null
+  }
+
+  function handleCloseBanner() {
+    setClosed(true)
+    localStorage.setItem(localStorageKeys.launchBannerClosed, 'true')
+  }
+
   return (
-    <Banner className="LaunchBanner">
+    <Banner className="LaunchBanner" onClose={handleCloseBanner}>
       The Web3Inbox app is live! Subscribe to Web3Inbox to get your Soundwaves NFT airdrop.{' '}
       <Link to={LAUNCH_BANNER_URL} target="_blank">
         <span className="LaunchBanner__learn-more">
