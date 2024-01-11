c668dd278a01305b2520bdf00b42d49c9336a7cd
enesozturk <enesozturk.d@gmail.com>
2024-01-11 10:26:04 +0300

    chore: update modal content layout

--- src/components/utils/NotificationPwaModal/index.tsx
+++ src/components/utils/NotificationPwaModal/index.tsx
@@ -46,26 +46,20 @@ export const NotificationPwaModal: React.FC = () => {
         <div className="NotificationPwaModal__icon">
           <img alt="Web3Inbox icon" className="wc-icon" src="/icon.png" />
         </div>
-        <div className="NotificationPwaModal__header">
-          <Text variant={'large-500'}>Enable Notifications</Text>
-        </div>
-        <div className="NotificationPwaModal__description">
-          <Text variant="small-500">
-            To use Web3Inbox and receive notifications from your subscribed apps, please enable push
-            notifications.
-          </Text>
-        </div>
+        <Text variant={'large-500'}>Enable Notifications</Text>
+        <Text variant="small-500">
+          To use Web3Inbox and receive notifications from your subscribed apps, please enable push
+          notifications.
+        </Text>
         {explicitlyDeniedPermissionForNotifications ? (
           <Text variant="small-700" className="NotificationPwaModal__warning">
             You have explicitly denied notification permissions. Please adjust in OS settings.
           </Text>
         ) : (
           <Fragment>
-            <div className="NotificationPwaModal_subtitle">
-              <Text variant="small-500">
-                You can always adjust your permissions in your OS settings.
-              </Text>
-            </div>
+            <Text variant="small-500">
+              You can always adjust your permissions in your OS settings.
+            </Text>
             <Button onClick={handleEnableNotifications} size="small">
               Enable Notifications
             </Button>
