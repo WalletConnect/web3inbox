bbc272b72deef26c0144607f1c9072b5d87aa11f
enesozturk <enesozturk.d@gmail.com>
2024-01-12 17:24:36 +0300

    chore: add event handlers on sw to handle notifs

--- src/main-sw.ts
+++ src/main-sw.ts
@@ -12,18 +12,22 @@ const clearCache = () => {
 
 // Clear any cache
 self.oninstall = () => {
+  console.log('>>> SW: oninstall')
   clearCache()
 }
 
 self.onactivate = () => {
+  console.log('>>> SW: oninactive')
   clearCache()
 }
 
 self.ononline = () => {
+  console.log('>>> SW: ononline')
   clearCache()
 }
 
 self.addEventListener('message', event => {
+  console.log('>>> SW: message', event?.data)
   if (!event.data) return
 
   switch (event.data.type) {
@@ -32,6 +36,27 @@ self.addEventListener('message', event => {
       self.skipWaiting()
       break
   }
+})
+
+// In your service worker
+self.addEventListener('push', event => {
+  console.log('>>> SW: push event', event)
+  const data = event?.data?.json()
+  // Store data for the app to access later
+  // Show a notification
+  self.registration.showNotification(data.title, {
+    body: data.body
+    // other options
+  })
+})
+
+self.addEventListener('notificationclick', event => {
+  console.log('>>> SW: notificationclick event', event)
+  event.notification.close()
+
+  event.waitUntil(
+    self.clients.openWindow('/') // Opens your app
+  )
 })
 
 self.addEventListener('install', () => {
