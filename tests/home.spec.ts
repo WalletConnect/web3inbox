630290ea0f147241b5f1aa5beb383b7cca5c96ea
Ben Kremer <ben@walletconnect.com>
2024-01-12 15:18:03 +0100

    chore: apply all prettier fixes

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,11 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test'
 
 test('has title', async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle(/Web3Inbox/);
-});
+  await page.goto('/')
+  await expect(page).toHaveTitle(/Web3Inbox/)
+})
 
 test('welcome message', async ({ page }) => {
-  await page.goto("/");
-  await expect(page.getByText("Welcome to Web3Inbox")).toBeVisible();
-});
+  await page.goto('/')
+  await expect(page.getByText('Welcome to Web3Inbox')).toBeVisible()
+})
