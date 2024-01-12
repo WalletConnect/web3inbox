a8a00e075418ac9176a1221a87dfb3e16ce458ab
Derek <alexanderderekrein@gmail.com>
2024-01-12 09:14:27 -0500

    feat: tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,19 +17,17 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  retries: 0,
+  /* Parallel tests currently blocked. */
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL,
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
   },
