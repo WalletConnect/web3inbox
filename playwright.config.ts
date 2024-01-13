aba6571d6c73dee3b20333e82aaf48afcebb492a
Celine Sarafa <celinesarafa@gmail.com>
2024-01-13 18:24:35 +0100

    Fix/force watch subscriptions (#393)
    * chore: add unregister functionality
    
    * chore: add unregister to facade
    
    * chore: add unregister others
    
    * chore: run prettier

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }
