bc92ed985378f04e414861f02182fbe793332eee
Derek <alexanderderekrein@gmail.com>
2024-01-15 08:51:37 -0500

    chore: udpdate dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig, devices } from '@playwright/test'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-
 const baseURL = 'http://localhost:5173'
 
 /**
