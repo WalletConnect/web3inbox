8474bf20dcd1b427fbfd193e224f3ab882f0e8e8
enesozturk <enesozturk.d@gmail.com>
2024-01-15 21:12:17 +0300

    chore: update pwa variables and comments

--- src/utils/pwa.ts
+++ src/utils/pwa.ts
@@ -4,14 +4,12 @@ export const isMobileBrowser = /iPhone|iPad|iPod|Android/i.test(navigator.userAg
 
 export const isAppleMobile = /iPhone|iPad|iPod/i.test(navigator.userAgent)
 
-export const isNonSafari = /CriOS|FxiOS/i.test(navigator.userAgent)
-
 /**
- * It's not possible to detect Safari on iOS. `navigator` object doesn't have enough information to detect if the running browser is exactly Safari.
- * There are several fields like `vendor` (deprecated), `userAgent`, `userAgentData`. But these values can mislead us since they can return the same values for different browsers.
- * Instead of detecting Safari and doing conditional renderings with this value, prefer different solution.
- * Tested mobile browsers: Safari, Chrome, Firefox, Arc, Brave
+ * WARN: Browser check can be tricky and not reliable at some cases.
+ * Some non-Safari browsers can return the same `userAgent` value as Safari, so it might not be quite possible to detect if the browser is Safari or not.
+ * There are several fields like `vendor` (deprecated), `userAgent`, `userAgentData`. Some of these are deprecated and some are returning similar values across different browsers like `userAgent`.
+ * So be aware that is not perfect solution.
  */
-export const isSafari = isAppleMobile && navigator.userAgent.match(/Safari/i)
+export const isNonSafari = /CriOS|FxiOS/i.test(navigator.userAgent)
 
 export const isMobileButNotInstalledOnHomeScreen = isMobileBrowser && !isInstalledOnHomeScreen
