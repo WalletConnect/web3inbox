b637685bdb7c3980f90ebeb9bbc65eab685d1188
enesozturk <enesozturk.d@gmail.com>
2024-01-15 18:38:05 +0300

    chore: update faqs url

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,6 +1,6 @@
 export const web3InboxURLs = {
   discord: 'https://discord.gg/B9mQsgE8pj',
   website: 'https://web3inbox.com',
-  websiteFaqs: 'https://web3inbox.com/#faq',
+  websiteFaqs: 'https://web3inbox.com/#faqs',
   walletConnectWeb3Inbox: 'https://walletconnect.com/web3inbox?utm_source=website&utm_medium=w3iapp'
 }
