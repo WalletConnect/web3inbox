83c51eec004e0116cc69dfbfa9fb6c025e595804
enesozturk <enesozturk.d@gmail.com>
2024-01-16 19:40:00 +0300

    refactor: update layout styles

--- src/components/layout/Sidebar/index.tsx
+++ src/components/layout/Sidebar/index.tsx
@@ -50,7 +50,7 @@ const Sidebar: React.FC<{ isLoggedIn: boolean }> = ({ isLoggedIn }) => {
 
     if (uiEnabled.settings) {
       items.push([
-        <SettingIcon isFilled={pathname.startsWith(NAVIGATION.settings.notification)} />,
+        <SettingIcon isFilled={pathname.startsWith(NAVIGATION.settings.index)} />,
         NAVIGATION.settings.index
       ])
     }
