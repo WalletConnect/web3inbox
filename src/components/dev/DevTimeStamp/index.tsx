44b5e92d18360b774b37b57e4ca8e18360b94b85
Celine Sarafa <celinesarafa@gmail.com>
2024-02-06 19:31:41 +0100

    chore: add timestamp formatting

--- src/components/dev/DevTimeStamp/index.tsx
+++ src/components/dev/DevTimeStamp/index.tsx
@@ -2,6 +2,26 @@ import { useContext, useEffect, useState } from 'react';
 import './DevTimeStamp.scss'
 import SettingsContext from '@/contexts/SettingsContext/context';
 
+const getTimeStampFormatted = (rawTimestamp: number) => {
+
+  const timestamp = new Date(rawTimestamp)
+
+  const year = timestamp.getFullYear();
+  const month = (timestamp.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-indexed
+  const day = timestamp.getDate().toString().padStart(2, '0');
+  const hours = timestamp.getHours().toString().padStart(2, '0');
+  const minutes = timestamp.getMinutes().toString().padStart(2, '0');
+  const seconds = timestamp.getSeconds().toString().padStart(2, '0');
+  
+  const timeZone = new Intl.DateTimeFormat('en', { timeZoneName: 'short' }).formatToParts(timestamp).find(part => part.type === 'timeZoneName')!.value;
+
+// Combine components into the desired format
+  const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${timeZone}`;
+
+  return formattedDateTime
+
+}
+
 const DevTimeStamp = () => {
   const [timestamp, setTimestamp] = useState(Date.now())
   const {isDevModeEnabled} = useContext(SettingsContext)
@@ -10,7 +30,7 @@ const DevTimeStamp = () => {
     if(isDevModeEnabled) {
      const intervalId = setInterval(() => {
       setTimestamp(Date.now())
-     }, 1)
+     }, 100)
 
      return () => clearInterval(intervalId)
     }
@@ -22,7 +42,7 @@ const DevTimeStamp = () => {
 
   return (
     <div className="DevTimeStamp">
-      <p>{timestamp}</p>
+      <p>{getTimeStampFormatted(timestamp)}</p>
     </div>
   )
 }
