4a2251d8b47227d05e77c3b22e377e84e44d5ed0
Celine Sarafa <celinesarafa@gmail.com>
2024-02-06 20:47:48 +0100

    chore: add dev time stamp component (#431)
    * chore: add dev time stamp component
    
    * chore: add timestamp formatting
    
    * chore: decrease interval
    
    * Update src/components/dev/DevTimeStamp/DevTimeStamp.scss
    
    Co-authored-by: Chris Smith <1979423+chris13524@users.noreply.github.com>
    
    * chore: move dev timestamp out of auth protected
    
    * chore: center timestamp
    
    * chore: place timestamp on top
    
    * chore: explain comment
    
    ---------
    
    Co-authored-by: Chris Smith <1979423+chris13524@users.noreply.github.com>

---
+++ src/components/dev/DevTimeStamp/index.tsx
@@ -0,0 +1,51 @@
+import { useContext, useEffect, useState } from 'react';
+import './DevTimeStamp.scss'
+import SettingsContext from '@/contexts/SettingsContext/context';
+
+const getTimeStampFormatted = (rawTimestamp: number) => {
+
+  const timestamp = new Date(rawTimestamp)
+
+  const year = timestamp.getFullYear();
+
+  // Months are zero-indexed
+  const month = (timestamp.getMonth() + 1).toString().padStart(2, '0'); 
+  const day = timestamp.getDate().toString().padStart(2, '0');
+  const hours = timestamp.getHours().toString().padStart(2, '0');
+  const minutes = timestamp.getMinutes().toString().padStart(2, '0');
+  const seconds = timestamp.getSeconds().toString().padStart(2, '0');
+  
+  const timeZone = new Intl.DateTimeFormat('en', { timeZoneName: 'short' }).formatToParts(timestamp).find(part => part.type === 'timeZoneName')!.value;
+
+  const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${timeZone}`;
+
+  return formattedDateTime
+
+}
+
+const DevTimeStamp = () => {
+  const [timestamp, setTimestamp] = useState(Date.now())
+  const {isDevModeEnabled} = useContext(SettingsContext)
+
+  useEffect(() => {
+    if(isDevModeEnabled) {
+     const intervalId = setInterval(() => {
+      setTimestamp(Date.now())
+     }, 250)
+
+     return () => clearInterval(intervalId)
+    }
+  }, [setTimestamp, isDevModeEnabled])
+
+  if (!isDevModeEnabled) {
+    return null;
+  }
+
+  return (
+    <div className="DevTimeStamp">
+      <span>{getTimeStampFormatted(timestamp)}</span>
+    </div>
+  )
+}
+
+export default DevTimeStamp;
