8ab816b01f0ca4ef0149663e97b266c7e37177f1
Chris Smith <chris@walletconnect.com>
2024-03-27 08:47:17 -0400

    fix: long test start

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn playwright:start',
+    command: process.env.CI ? 'yarn preview' : 'VITE_CI=true yarn dev',
     url: baseURL,
     reuseExistingServer: !process.env.CI
   }
