6fcc5184aaa611079f52829250393b1c271b94a5
Chris Smith <chris@walletconnect.com>
2024-04-08 16:49:41 -0400

    fix: disable Safari for now

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,10 +39,10 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }
     },
-    {
-      name: 'safari',
-      use: { ...devices['Desktop Safari'] }
-    }
+    // {
+    //   name: 'safari',
+    //   use: { ...devices['Desktop Safari'] }
+    // }
   ],
 
   /* Run your local dev server before starting the tests */
