0c23c8de469bef2aa10f0e959b6ec71efa1b52d8
Celine Sarafa <celinesarafa@gmail.com>
2024-04-29 17:28:31 +0200

    refactor: move siwe logic to own file

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { getAccount } from '@wagmi/core'
-import { composeDidPkh } from '@walletconnect/did-jwt'
 import { Web3InboxClient } from '@web3inbox/core'
 import { initWeb3InboxClient } from '@web3inbox/react'
 import {
-  SIWEVerifyMessageArgs,
-  createSIWEConfig,
-  formatMessage,
-  getAddressFromMessage,
-  getChainIdFromMessage
+  createSIWEConfig
 } from '@web3modal/siwe'
 import { createWeb3Modal } from '@web3modal/wagmi/react'
 import ReactDOM from 'react-dom/client'
@@ -30,7 +24,7 @@ import { Modals } from './Modals'
 import DevTimeStamp from './components/dev/DevTimeStamp'
 
 import './index.css'
-import { waitFor } from './utils/general'
+import { createMessage, getMessageParams, getNonce, getSession, verifyMessage } from './utils/siwe'
 
 polyfill()
 initSentry()
@@ -42,151 +36,13 @@ if (!projectId) {
   throw new Error('VITE_PROJECT_ID is required')
 }
 
-const verifySiweMessage = async (params: SIWEVerifyMessageArgs) => {
-  if (!client) {
-    throw new Error('Failed to verify message - no client')
-  }
-
-  if (!registerParams) {
-    throw new Error('Failed to verify message - no registerParams saved')
-  }
-
-  // Start signing the signature modal so it does not show up
-  // in sign 2.5
-  const account = composeDidPkh(
-    `${getChainIdFromMessage(params.message)}:${getAddressFromMessage(params.message)}`
-  )
-
-  if (await client.getAccountIsRegistered(account)) {
-    return true
-  }
-
-  // Unregister account if registered with a faulty registration.
-  try {
-    await client.unregister({ account })
-  } catch (e) {}
-
-  console.log({ registerParams, paramsCacao: params.cacao?.p })
-
-  await client.register({
-    registerParams: {
-      allApps: true,
-      cacaoPayload: {
-        ...registerParams.cacaoPayload,
-        ...params.cacao?.p,
-        iss: account
-      },
-      privateIdentityKey: registerParams.privateIdentityKey
-    },
-    signature: params.signature
-  })
-
-  return true
-}
-
-let registerParams: Awaited<ReturnType<Web3InboxClient['prepareRegistrationViaRecaps']>> | null =
-  null
-let verifyingMessage = false;
 
 const siweConfig = createSIWEConfig({
-  getMessageParams: async () => {
-    if (!client) {
-      throw new Error('Client not ready yet')
-    }
-
-    registerParams = await client.prepareRegistrationViaRecaps({
-      domain: window.location.hostname,
-      allApps: true
-    })
-
-    const { cacaoPayload } = registerParams
-
-    return {
-      chains: wagmiConfig.chains.map(chain => chain.id),
-      domain: cacaoPayload.domain,
-      statement: cacaoPayload.statement ?? undefined,
-      uri: cacaoPayload.uri,
-      resources: cacaoPayload.resources
-    }
-  },
-  createMessage: ({ address, ...args }) => {
-    if (!registerParams) {
-      throw new Error("Can't create message if registerParams is undefined")
-    }
-
-    registerParams = {
-      ...registerParams,
-      cacaoPayload: {
-        ...registerParams?.cacaoPayload,
-        ...args
-      }
-    }
-
-    const message = formatMessage(registerParams.cacaoPayload, address)
-
-    // statement is generated in format message and not part of original payload.
-    const statement = message.split('\n')[3]
-    registerParams.cacaoPayload.statement = statement
-
-    console.log(
-      '>>> formatted message',
-      message,
-      'with params',
-      registerParams.cacaoPayload,
-      'and overrides',
-      args
-    )
-
-    return message
-  },
-  getNonce: async () => {
-    console.log('>>> getNonce')
-    return registerParams?.cacaoPayload.nonce ?? 'FAILED_NONCE'
-  },
-  getSession: async () => {
-    const { address, chainId } = getAccount({ ...wagmiConfig })
-
-    console.log('>>> getSession')
-
-    const account = `eip155:${chainId}:${address}`
-    console.log('>>> getSession for account', account)
-
-    const identityKey = await client?.getAccountIsRegistered(account)
-
-    const invalidSession = !(address && chainId && identityKey);
-
-    if (invalidSession) throw new Error("Failed to get account")
-
-    return {
-      address,
-      chainId,
-    }
-  },
-  verifyMessage: async params => {
-    try {
-      verifyingMessage = true;
-      console.log(">>>>>>>>> OOOOOOOOOOOOOOOOOOOO verifying")
-      const messageIsValid = await verifySiweMessage(params)
-
-      const account = `${getChainIdFromMessage(params.message)}:${getAddressFromMessage(params.message)}`;
-
-      console.log(">>>>>>>>> [][][][]", { messageIsValid })
-
-      if(messageIsValid) {
-	await waitFor(() => client!.getAccountIsRegistered(account))
-      }
-
-      verifyingMessage = false;
-
-      console.log(">>>>>> [][][][] ABSOLUTELY RETURNING, NOT THROWING", { messageIsValid })
-      
-      return messageIsValid
-    } catch (e) {
-      console.log(">>>>>>>>> [][][][] verifying failed", e)
-      verifyingMessage = false;
-      return false
-    }
-  },
+  getMessageParams: () => getMessageParams(client),
+  createMessage: (params) => createMessage(params),
+  getNonce: () => getNonce(),
+  getSession: () => getSession(client),
+  verifyMessage: (params) => verifyMessage(params, client),
   signOut: () => Promise.resolve(true),
 })
 
